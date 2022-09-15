e05683e5162470d31ec116315b544f47d9e9763b
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2022-09-15 14:50:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import Split
 from subprocess import call
 import sys
 
-if (len(sys.argv) < 2):
-    print("Error\nFormat: \n\tpython main.py your-pdf-file")
-else:
-    filename = sys.argv[1]
-    directory = "splitted/" + filename
+if sys.argv[1][-4:] != ".pdf" :
+    exit("""\033[;31;m\033[;1;mError
+    Format:
+    \t\033[;32;m\033[;4;mpython main.py your-pdf-file\033[;0;m""")
 
-    Split.split(directory, filename)
-    pdfFileObj = open(filename, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+filename = sys.argv[1]
+directory = "splitted/" + filename
 
-    for i in range(pdfReader.numPages):
-        splitted_file_name = directory + "/" + repr(i)
-        call(["pdftotext", splitted_file_name + ".pdf"])
-        # f = open(splitted_file_name + '.txt', 'r')
-        # print("Page %s" % repr(i+1))
-        # print(f.read())
-        # print("====================")
+Split.split(directory, filename)
+pdfFileObj = open(filename, 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+for i in range(pdfReader.numPages):
+    splitted_file_name = directory + "/" + repr(i)
+    call(["pdftotext", splitted_file_name + ".pdf"])
+    # f = open(splitted_file_name + '.txt', 'r')
+    # print("Page %s" % repr(i+1))
+    # print(f.read())
+    # print("====================")
