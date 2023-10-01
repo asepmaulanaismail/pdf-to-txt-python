64d6da05178213f702621a6a08bd92c4faba1611
Devin Burnette <devin.burnette@betterment.com>
2023-10-01 13:59:18 -0400

    fix deprecation errors

--- Split.py
+++ Split.py
@@ -1,20 +1,20 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfWriter, PdfReader
 import os, errno
 
 filename = "long-sample.pdf"
 directory = "splitted/"+filename
 
 def split(directory, filename):
-    inputpdf = PdfFileReader(open(filename, "rb"))
+    inputpdf = PdfReader(open(filename, "rb"))
     try:
         os.makedirs(directory)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
 
-    for i in range(inputpdf.numPages):
-        output = PdfFileWriter()
-        output.addPage(inputpdf.getPage(i))
+    for i in range(len(inputpdf.pages)):
+        output = PdfWriter()
+        output.add_page(inputpdf.pages[i])
         with open(directory+ "/%s.pdf" % i, "wb") as outputStream:
             output.write(outputStream)
 
